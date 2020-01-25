a7a56f64441949473aa9336ab3cf788d45f6da23
KhalifaGad <kkhalifa.gad@gmail.com>
2020-01-25 14:03:29 +0200

    reading from terminal

--- algos/linearSearch.js
+++ algos/linearSearch.js
@@ -1,3 +1,5 @@
+const readline = require('readline')
+
 /* 
 linear search or sequential search is a method for finding an element within a list.
 It sequentially checks each element of the list until a match is found 
@@ -9,11 +11,24 @@ or the whole list has been searched
 // Best-case performance: O(1)
 
 
-function linearSearch(arr, searchingKey, index = 0) {
+function linearSearch(arr, searchingVal, index = 0) {
     if (!arr.length) return -1
     const [head, ...tail] = arr
-    return head == searchingKey ?
-        index : linearSearch(tail, searchingKey, index + 1)
+    return head == searchingVal ?
+        index : linearSearch(tail, searchingVal, index + 1)
 }
 
-console.log(linearSearch('hello world', 'w'))
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Hey this is linear search \nEnter the array:', (arr) => {
+    rl.question('Enter searching value:', (searchingVal) => {
+        rl.question('Starting index (default 0)', (index = 0) => {
+            if (!index) index = 0
+            console.log(linearSearch(JSON.parse(arr), searchingVal, index))
+            process.exit()
+        })
+    })
+})
