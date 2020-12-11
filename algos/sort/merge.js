0e61d86a20f0e017aad94eb4dcad448a8a4a2acd
KhalifaGad <khalifa.gad@inovaeg.com>
2020-12-11 13:06:45 +0200

    merge sort

---
+++ algos/sort/merge.js
@@ -0,0 +1,32 @@
+function merge(arr) {
+  arr = arr.map((el) => [el]);
+  let sortedArr = [];
+
+  while (sortedArr.length != 1) {
+    sortedArr = [];
+    for (let i = 0; i < arr.length; i += 2) {
+      sortedArr.push(arrange(arr[i], arr[i + 1]));
+    }
+    arr = sortedArr;
+  }
+  return arr[0];
+}
+
+function arrange(arr1, arr2) {
+  let arranged = [];
+  while (arr1.length && arr2.length) {
+    if (arr1[0] > arr2[0]) {
+      arranged.push(arr2.shift());
+    } else {
+      arranged.push(arr1.shift());
+    }
+  }
+
+  if (arr1.length > 0) arranged.push(...arr1);
+  if (arr2.length > 0) arranged.push(...arr2);
+
+  return arranged;
+}
+
+let arr = [52, 37, 63, 14, 17, 8, 6, 25];
+console.log(merge(arr));
